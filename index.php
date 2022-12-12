1db3d4ead591e6cfcbe7a0d6d67e125ad281da7b
OsamaEltabakh <osama.eltabakh007@gmail.com>
2022-12-12 23:28:39 +0200

    new

--- index.php
+++ index.php
@@ -106,7 +106,7 @@ if (isset ($_POST['submit'])) {
             </tr>
             <tr>
                 <td><label>Agree:</label></td>
-                <td><input type="checkbox" name="agree">
+                <td><input type="checkbox" name="agree" <?php echo $agree?> >
                 <span class="error">*<?php echo $agreeErr;?></span></td>
             </tr>
                 
