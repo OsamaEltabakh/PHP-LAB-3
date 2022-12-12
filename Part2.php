c3bef75f946f42009b5993a31398300b6f21f5ee
OsamaEltabakh <osama.eltabakh007@gmail.com>
2022-12-12 04:39:38 +0200

    part2

--- Part2.php
+++ Part2.php
@@ -5,9 +5,6 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Part 2</title>
-</head>
-<body>
-    
 
 <style>
 
@@ -34,8 +31,8 @@ border-radius: 10%;
 </style>
 <H2> PHP CLASS Registration </H2>
 </head>
+<body>
 
-<body >
 
 <?php
 
